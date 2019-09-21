927bc658517bd241efb8267caa66e1706ed305dc
sullycodes <dansullivan19@gmail.com>
2019-09-20 21:44:32 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'patients/index'
+
+  get 'patients/new'
+
+  get 'patients/create'
+
+  get 'patients/show'
+
+  get 'patients/edit'
+
+  get 'patients/update'
+
+  get 'dcotors/index'
+
+  get 'dcotors/show'
+
+  get 'dcotors/new'
+
+  get 'dcotors/edit'
+
+  get 'dcotors/update'
+
+  get 'dcotors/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
